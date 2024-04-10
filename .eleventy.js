3d5a24efa212914c822a88789b3690337d9cd5c8
Michael Sean Becker <michael@ritual.com>
2024-04-09 21:38:11 -0700

    App commit try again

--- .eleventy.js
+++ .eleventy.js
@@ -33,7 +33,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addNunjucksAsyncShortcode('image', require('./src/_11ty/imageShortcode').imageShortcode)
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`)
-  eleventyConfig.addShortcode("appVer", () => `${process.env.APP_VERSION}`)
+  eleventyConfig.addShortcode("appVer", () => `${process.env.APP_VERSION || '0.0.0'}`)
 
   eleventyConfig.addFilter('readableDate', dateObj => {
     return DateTime.fromJSDate(dateObj, {
