0b580740330314279858282f1fad857687ca35d4
Michael Sean Becker <michael@ritual.com>
2024-07-18 00:34:17 -0700

    Add data and include for next Panthers game #71

---
+++ src/_data/panthers/schedule2324.js
@@ -0,0 +1,11 @@
+const EleventyFetch = require("@11ty/eleventy-fetch");
+
+module.exports = async function () {
+	let url = "https://api-web.nhle.com/v1/club-schedule-season/FLA/20232024";
+
+	/* This returns a promise */
+	return EleventyFetch(url, {
+		duration: "1d", // save for 1 day
+		type: "json", // we’ll parse JSON for you
+	});
+};
--- src/_data/panthers/schedule2425.js
+++ src/_data/panthers/schedule2425.js
@@ -3,9 +3,19 @@ const EleventyFetch = require("@11ty/eleventy-fetch");
 module.exports = async function () {
 	let url = "https://api-web.nhle.com/v1/club-schedule-season/FLA/20242025";
 
-	/* This returns a promise */
-	return EleventyFetch(url, {
+	let season = await EleventyFetch(url, {
 		duration: "1d", // save for 1 day
 		type: "json", // we’ll parse JSON for you
 	});
+	/* This returns a promise */
+	// add a function to get the next game by date from season
+	season.getNextGame = function () {
+		// const now = new Date();
+		// const nextGame = this.data.find(game => new Date(game.date) > now);
+		// return nextGame;
+		return "Next game date";
+	};
+
+	console.log(season.getNextGame);
+	return season;
 };
